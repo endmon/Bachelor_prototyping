06cd167c51d48108d982f07bde9ff6146a1fe6a8
Endmon <miguel.lopesgouveia@heig-vd.ch>
2021-07-19 01:15:23 +0200

    Manage binary

--- geo_aggregation_cloudflare/src/lib.rs
+++ geo_aggregation_cloudflare/src/lib.rs
@@ -7,13 +7,14 @@ use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
 use serde_json::Value;
-use web_sys::{Request, RequestInit, Response};
+use web_sys::{Request, RequestInit, Response, Blob, FileReaderSync};
 use log::{info, Level};
 use js_sys::{global, Function, Object, Promise, Reflect, JsString};
 use urlparse::urlparse;
 use urlparse::GetQuery;  // Trait
 use worker_kv::*;
 use std::fmt::Binary;
+use wasm_bindgen::__rt::std::convert::TryInto;
 
 mod utils;
 
@@ -56,18 +57,45 @@ async fn fetch_rust_wasm(url:&str) -> Result<String, JsValue> {
     Ok(responce_string)
 }
 
+async fn fetch_rust_wasm_binary(url:&str) -> Result<String, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+
+    let request = Request::new_with_str_and_init(url, &opts)?;
+
+    let global = worker_global_scope().unwrap();
+    let responce = JsFuture::from(global.fetch_with_request(&request)).await?;
+    let responce_value: Response = responce.dyn_into().unwrap();
+    let responce_json = JsFuture::from(responce_value.blob()?).await?;
+    //let responce_string = responce_json.as_string().unwrap();
+    //let v:Vec<u8> = responce_json.try_into().unwrap();
+    let responce_blob = Blob::new_with_buffer_source_sequence(&responce_json);
+    let mut reps = "".to_string();
+    if responce_blob.is_ok() {
+        reps = "t".to_string();
+    } else {
+        reps = "nope".to_string();
+    }
+
+    //let responce_string = responce_json.as_string().unwrap();
+
+    Ok(reps.to_string())
+}
+
 #[wasm_bindgen]
 pub async fn test(request:Request) -> Result<String, JsValue> {
 
-    let url = urlparse(request.url()); //retourne un string de type https://example.com/example
+    //let url = urlparse(request.url()); //retourne un string de type https://example.com/example
 
     let mut result = "".to_string();
-    let mut v: Vec<&str> = url.path.rsplit("/").collect();
+    //let mut v: Vec<&str> = url.path.rsplit("/").collect();
 
-    v.pop();
-    let mut layers = v.pop().unwrap();
+    //v.pop();
+    //let mut layers = v.pop().unwrap();
 
-    let mut vector_layers:Vec<&str> = layers.rsplit("+").collect();
+    //let mut vector_layers:Vec<&str> = layers.rsplit("+").collect();
+
+    //result = fetch_rust_wasm_binary("http://miguel-gouveia.me/a").await.unwrap();
 
     //let mut file:Binary;
 
--- geo_aggregation_cloudflare/worker/worker.js
+++ geo_aggregation_cloudflare/worker/worker.js
@@ -3,11 +3,39 @@ addEventListener('fetch', event => {
 })
 
 
+async function gatherResponse(response) {
+    const { headers } = response
+    const contentType = headers.get("content-type") || ""
+    if (contentType.includes("application/json")) {
+        return await response.blob()
+    }
+    else if (contentType.includes("application/text")) {
+        return await response.blob()
+    }
+    else if (contentType.includes("text/html")) {
+        return await response.blob()
+    }
+    else {
+        return await response.blob()
+    }
+}
+
 /**
  * Fetch and log a request
  * @param {Request} request
  */
 async function handleRequest(request) {
+
+    const init = {
+        headers: {
+            "content-type": "application/vnd.mapbox-vector-tile",
+        },
+    }
+
+    //JSON.stringify(greeting);
+    const response_a = await fetch("http://miguel-gouveia.me/geo/14/8494/5777.pbf", init)
+    const results_a = await gatherResponse(response_a)
+    var result_size = results_a.size
 
     const { test } = wasm_bindgen;
     await wasm_bindgen(wasm)
@@ -17,5 +45,5 @@ async function handleRequest(request) {
     //let val = await user_KV.get("M. Test");
 
     //return new Response(JSON.stringify(greeting), {status: 200})
-    return new Response(greeting, {status: 200})
+    return new Response(results_a, {status: 200, headers: { "content-type": "aplication/vnd.mapbox-vector-tile",},})
 }
